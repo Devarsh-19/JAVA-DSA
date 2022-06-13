038350aa6e6b61cb4cb6a38a40f3cf92ddb446b5
Devarsh-19 <devarshraj5@gmail.com>
2022-06-13 20:01:58 +0530

    commited on 13-6-22

---
+++ README.md
@@ -0,0 +1,12 @@
+# DSA
+
+CONTAINS JAVA DSA IMPLEMENTAION AND BASIC PROBLEMS RELATED
+
+Completed Topics:
+
+- Arrays
+- Linked Lists
+    - Singly
+    - Circular
+    - Doubly
+- 
--- src/recursion/SubSequence.java
+++ src/recursion/SubSequence.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import collections_framework.Arraylist;
+
 public class SubSequence {
     public static void main(String[] args) {
         String nm = "ABC";
@@ -10,6 +12,20 @@ public class SubSequence {
     static void sub(String p, String up) {
         if (up.isEmpty()) {
             System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        sub(p, up.substring(1));
+        sub(p + ch, up.substring(1));
+    }
+
+
+    static Arraylist<String> sub1(String p, String up, Arraylist<String> s) {
+
+        if (up.isEmpty()) {
+            s.
+                    System.out.println(p);
             return;
         }
 
