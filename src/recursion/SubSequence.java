378918693ac20133b8f47ef9afc40ab6952981a8
Devarsh-19 <devarshraj5@gmail.com>
2022-06-13 19:53:08 +0530

    commited on 13-6-22

---
+++ src/recursion/SubSequence.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class SubSequence {
+    public static void main(String[] args) {
+        String nm = "ABC";
+
+        sub("", nm);
+    }
+
+    static void sub(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        sub(p, up.substring(1));
+        sub(p + ch, up.substring(1));
+    }
+}
